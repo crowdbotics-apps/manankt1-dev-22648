632dde9b2a7cdfe3545c78585ceabdc9f46e10cc
Crowdbotics <team@crowdbotics.com>
2021-06-29 13:05:12 +0000

    Update models

--- backend/manankt1_dev_22648/wsgi.py
+++ backend/manankt1_dev_22648/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'manankt1_dev_22648.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "manankt1_dev_22648.settings")
 
 application = get_wsgi_application()
